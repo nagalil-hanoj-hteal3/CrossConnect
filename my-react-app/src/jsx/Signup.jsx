d774b461d1793cc6456b166e6af3cbe036b295e6
asedadus1 <asedadus1@gmail.com>
2023-12-20 11:48:01 -0800

    match my branch with main

--- my-react-app/src/jsx/Signup.jsx
+++ my-react-app/src/jsx/Signup.jsx
@@ -1,27 +1,48 @@
 import React from "react";
 import "../css/signup.css";
-import LoginBackground from "../assets/LoginBackground.jpg";
 
-
-const Signup = () => {
-    return (
-        <div className="signup">
-          <div className="container">
-            <div className="input-session">
-                <div className="text-wrapper">
-                    <h1>CrossConnect</h1>
-                </div>
-                <input type="text" name="fullname" id="fullname" placeholder="First name - Last name" />
-                <input type="text" name="username" id="username" placeholder="Create Username" />
-                <input type="email" name="email" id="email" placeholder="Enter your email" />
-                <input type="password" name="password" id="password" placeholder="Create your password" />
-                <input type="password" name="password" id="password" placeholder="Re-enter your password" />
-
-                <button className="sign-up">Sign up</button>
-            </div>
+const SignUp = () => {
+  return (
+    <div className="signup">
+      <img className="loginbackground" alt="Loginbackground" src="loginbackground-2-1.png" />
+      <div className="sign-up">
+        <div className="text-wrapper">Sign Up</div>
+      </div>
+      <div className="input-session">
+        <div className="password">
+          <div className="overlap-group">
+            <div className="div">Enter your password</div>
+          </div>
+          <div className="text-wrapper-2">Password</div>
+        </div>
+        <div className="confirm-password">
+          <div className="overlap-group">
+            <div className="div">Re Enter your password</div>
+          </div>
+          <div className="text-wrapper-2">Password</div>
+        </div>
+        <div className="email">
+          <div className="overlap-group">
+            <div className="text-wrapper-3">Enter your Email Address</div>
+          </div>
+          <div className="text-wrapper-2">Email Address</div>
+        </div>
+        <div className="user-name">
+          <div className="overlap-group">
+            <div className="text-wrapper-3">Create Username</div>
+          </div>
+          <div className="text-wrapper-2">Username</div>
+        </div>
+        <div className="full-name">
+          <div className="text-wrapper-4">Full Name</div>
+          <div className="frame">
+            <p className="p">First name - Last name</p>
           </div>
         </div>
-    );
+      </div>
+      <div className="text-wrapper-5">CrossConnect</div>
+    </div>
+  );
 };
 
-export default Signup;
+export default SignUp;
