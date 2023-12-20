18806e733da5349e77d31ac12cc9793b2fba609d
nagalil-honaj-hteal3 <elaeth.jonah@gmail.com>
2023-12-20 15:16:28 -0800

    my changes for the sign up page

--- my-react-app/src/jsx/SignUp.jsx
+++ my-react-app/src/jsx/SignUp.jsx
@@ -1,47 +1,67 @@
 import React from "react";
 import "../css/signup.css";
+import { useNavigate } from 'react-router-dom';
+import SignupBackground from "../assets/SignUp.jpg";
 
 const SignUp = () => {
+    const navigate = useNavigate();
+
+    // Function to handle navigation to the signup page
+    const navigateToLogin = () => {
+        navigate('/'); // Use navigate to go to the signup page
+    };
+
   return (
-    <div className="signup">
-      <img className="loginbackground" alt="Loginbackground" src="loginbackground-2-1.png" />
-      <div className="sign-up">
-        <div className="text-wrapper">Sign Up</div>
-      </div>
-      <div className="input-session">
+<div className="signup">
+    <div className="overlap">
+        <p className="text-wrapper" onClick={navigateToLogin}>Already have an account? Log in</p>
+
+        <button className="sign-up" onClick={navigateToLogin}>
+            <div className="div">Sign Up</div>
+        </button>
+
+        <div className="input-session">
+            <div className="confirm-password">
+                <div className="overlap-group">
+                    <input className="text-wrapper-2" type="password" name="confirmPassword" id="confirmPassword" placeholder="Retype your password"/>
+                </div>
+            <div className="text-wrapper-3">Confirm Password</div>
+        </div>
+
         <div className="password">
-          <div className="overlap-group">
-            <div className="div">Enter your password</div>
-          </div>
-          <div className="text-wrapper-2">Password</div>
+            <div className="overlap-group">
+                <input className="text-wrapper-2" type="password" name="password" id="password" placeholder="Enter your password"/>
+                {/* </div> */}
+            </div>
+                <div className="text-wrapper-3">Password</div>
         </div>
-        <div className="confirm-password">
-          <div className="overlap-group">
-            <div className="div">Re Enter your password</div>
-          </div>
-          <div className="text-wrapper-2">Password</div>
-        </div>
+
         <div className="email">
-          <div className="overlap-group">
-            <div className="text-wrapper-3">Enter your Email Address</div>
-          </div>
-          <div className="text-wrapper-2">Email Address</div>
+            <div className="overlap-group">
+                <input className="text-wrapper-4" type="email" name="email" id="email" placeholder="Enter your email address"/>
+            </div>
+                <div className="text-wrapper-3">Email Address</div>
         </div>
+
         <div className="user-name">
-          <div className="overlap-group">
-            <div className="text-wrapper-3">Create Username</div>
-          </div>
-          <div className="text-wrapper-2">Username</div>
+            <div className="overlap-group">
+                <input className="text-wrapper-4" type="text" name="username" id="username" placeholder="Enter Username"/>
+            </div>
+                <div className="text-wrapper-3">Username</div>
         </div>
+
         <div className="full-name">
-          <div className="text-wrapper-4">Full Name</div>
-          <div className="frame">
-            <p className="p">First name - Last name</p>
-          </div>
+            <div className="overlap-group">
+                <input className="text-wrapper-4" type="text" name="username" id="username" placeholder="Enter Full Name"/>
+            </div>
+                <div className="text-wrapper-3">Full Name</div>
+            </div>
         </div>
-      </div>
-      <div className="text-wrapper-5">CrossConnect</div>
+
+        <div className="text-wrapper-5">CrossConnect</div>
     </div>
+    <img className="img" alt="Signup" src={SignupBackground} />
+</div>
   );
 };
 
