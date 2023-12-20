96430552dfb25b147178bb71b94fa27954331292
nagalil-honaj-hteal3 <elaeth.jonah@gmail.com>
2023-12-19 23:15:16 -0800

    signup page blank, changed main.jsx

--- my-react-app/src/jsx/main.jsx
+++ my-react-app/src/jsx/main.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-import '../css/login.css'
-import Login from '../jsx/Login.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Import from react-dom/client
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from '../jsx/Login.jsx';
+import SignUp from '../jsx/SignUp.jsx';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-  <Login></Login>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
