57896c770c884fb1ce6cda26c715de2f17272d51
nagalil-honaj-hteal3 <elaeth.jonah@gmail.com>
2023-12-20 23:04:44 -0800

    final touches with login and create account frontend pages

--- my-react-app/frontend/src/jsx/Login.jsx
+++ my-react-app/frontend/src/jsx/Login.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import "../css/login.css";
 import LoginBackground from "../assets/LoginBackground.jpg";
+import eye from "../assets/eye.png";
+import closedEye from "../assets/eye-close.png";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
 
   // Function to handle navigation to the signup page
   const navigateToSignUp = () => {
     navigate('/signup'); // Use navigate to go to the signup page
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
   };
   
   return (
@@ -37,7 +44,14 @@ const Login = () => {
 
         <div className="password">
           <div className="div-wrapper">
-          <input className="text-wrapper-4" type="password" name="password" id="password" placeholder="Enter your password"/>
+            <input className="text-wrapper-4" type={showPassword ? "text" : "password"} name="password" id="password" placeholder="Enter your password"/>
+            <div className="eye-icon" onClick={togglePasswordVisibility}>
+              <img
+                src={showPassword ? eye : closedEye}
+                alt={showPassword ? "EyeIcon" : "ClosedEyeIcon"}
+                className="eye-img"
+              />
+            </div>
           </div>
           <div className="text-wrapper-5">Password</div>
         </div>
@@ -60,66 +74,3 @@ const Login = () => {
 };
 
 export default Login;
-// import React from "react";
-// import { useNavigate } from 'react-router-dom';
-// import "../css/login.css";
-// import LoginBackground from "../assets/LoginBackground.jpg";
-
-// const Login = () => {
-//   const navigate = useNavigate();
-
-//   // Function to handle navigation to the signup page
-//   const navigateToSignUp = () => {
-//     navigate('/signup'); // Use navigate to go to the signup page
-//   };
-//   return (
-    
-//     <div className="login">
-//       <img className="loginbackground" alt="Loginbackground" src={LoginBackground} />
-      
-//       <button className="sign-up" onClick={navigateToSignUp}>
-//         <div className="overlap-group">
-//           <div className="text-wrapper">
-//             Sign Up
-//           </div>
-//         </div>
-//       </button>
-
-//       <button className="login-button">
-//         <div className="overlap-group2">
-//           <div className="div">Login</div>
-//         </div>
-//       </button>
-
-//       <div className="text-wrapper-2">Forgot Password</div>
-      
-//       <div className="remember">
-//         <input type="checkbox" className="rectangle" />
-//         <div className="text-wrapper-3">Remember Me</div>
-//       </div>
-
-//       <div className="password">
-//         <div className="overlap">
-//           <div className="text-wrapper-4">
-//             <input type="password" name="password" id="password" placeholder="Enter your password"></input>
-//           </div>
-//         </div>
-//         <div className="text-wrapper-5">Password</div>
-//       </div>
-
-//       <div className="email-address">
-//         <div className="overlap">
-//           <div className="text-wrapper-6">
-//             <input type="email" name="email" id="email" placeholder="Enter your email"></input>
-//           </div>
-//         </div>
-//         <div className="text-wrapper-5">Email Address</div>
-//       </div>
-
-//       {/* <div className="text-wrapper-7">Good to see you</div> */}
-//       <div className="text-wrapper-8">CrossConnect</div>
-//     </div>
-//   );
-// };
-
-// export default Login;
