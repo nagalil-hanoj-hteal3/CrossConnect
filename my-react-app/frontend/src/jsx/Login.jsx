4004e77c1bb7831846d88cbf927545dc789556cc
asedadus1 <asedadus1@gmail.com>
2023-12-21 10:26:17 -0800

    login validation form & fixed signup page

--- my-react-app/frontend/src/jsx/Login.jsx
+++ my-react-app/frontend/src/jsx/Login.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import "../css/login.css";
 import LoginBackground from "../assets/LoginBackground.jpg";
+import Validation from "./loginValidation";
+
 
 const Login = () => {
   const navigate = useNavigate();
@@ -10,12 +12,28 @@ const Login = () => {
   const navigateToSignUp = () => {
     navigate('/Signup'); // Use navigate to go to the signup page
   };
+  const [values, setValues] = useState({
+    email: '',
+    password: ''
+  })
+
+  const [errors, setErrors] = useState({});
+  const handleInput = (event) => {
+    setValues(prev => ({...prev, [event.target.name]: [event.target.values]}))
+  }
+
+  const handleSubmit=(event) =>{
+    event.preventDefault();
+    setErrors(Validation(values));
+  }
+
+
   return (
     
     <div className="login">
       <img className="loginbackground" alt="Loginbackground" src={LoginBackground} />
       
-      <button className="sign-up" onClick={navigateToSignUp}>
+      <button type='submit' className="sign-up" onClick={navigateToSignUp}>
         <div className="overlap-group">
           <div className="text-wrapper">
             Sign Up
@@ -23,11 +41,6 @@ const Login = () => {
         </div>
       </button>
 
-      <button className="login-button">
-        <div className="overlap-group2">
-          <div className="div">Login</div>
-        </div>
-      </button>
 
       <div className="text-wrapper-2">Forgot Password</div>
       
@@ -36,24 +49,38 @@ const Login = () => {
         <div className="text-wrapper-3">Remember Me</div>
       </div>
 
-      <div className="password">
-        <div className="overlap">
-          <div className="text-wrapper-4">
-            <input type="password" name="password" id="password" placeholder="Enter your password"></input>
+      <form action="" onSubmit={handleSubmit}>
+        <div className="password">
+          <div className="overlap">
+            <div className="text-wrapper-4">
+              <input type="password" name="password" id="password" placeholder="Enter your password" onChange={handleInput}></input>
+              <br></br>
+              <br></br>
+              {errors.password && <span className="text-danger"> {errors.password} </span>}
+            </div>
           </div>
+          <div className="text-wrapper-5">Password</div>
         </div>
-        <div className="text-wrapper-5">Password</div>
-      </div>
 
-      <div className="email-address">
-        <div className="overlap">
-          <div className="text-wrapper-6">
-            <input type="email" name="email" id="email" placeholder="Enter your email"></input>
+        <div className="email-address">
+          <div className="overlap">
+            <div className="text-wrapper-6">
+              <input type="email" name="email" id="email" placeholder="Enter your email" onChange={handleInput}></input>
+              <br></br>
+              <br></br>
+              {errors.email && <span className="text-danger"> {errors.email} </span>}
+            </div>
           </div>
+          <div className="text-wrapper-5">Email Address</div>
         </div>
-        <div className="text-wrapper-5">Email Address</div>
-      </div>
 
+        <button type="submit" className="login-button">
+          <div className="overlap-group2">
+            <div className="div">Login</div>
+          </div>
+        </button>
+
+        </form>
       {/* <div className="text-wrapper-7">Good to see you</div> */}
       <div className="text-wrapper-8">CrossConnect</div>
     </div>
